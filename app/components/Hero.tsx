1e94b7069cf3e068bc04ef81ef96bca5ecc3b381
abhijeetrajput103 <abhijeetrajput103@gmail.com>
2025-07-24 18:27:59 +0530

    Added Messages Section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,6 +3,9 @@ import { useGSAP } from "@gsap/react";
 import Image from "next/image";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(SplitText, ScrollTrigger);
 
 const Hero = () => {
   useGSAP(() => {
@@ -16,11 +19,36 @@ const Hero = () => {
       opacity: 1,
       y: 0,
       ease: "power1.inOut",
-    }).to(".hero-text-scroll", {
-      duration: 1,
-      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-      ease: "circ.out",
-    },"-=0.5");
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.out",
+        },
+        "-=0.5"
+      )
+      .from(titleSplit.chars, {
+        yPercent: 200,
+        stagger: 0.02,
+        ease: "power2.out",
+      });
+
+    const heroTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-container",
+        start: "1% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    heroTl.to(".hero-container", {
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    });
   });
 
   return (
@@ -31,6 +59,8 @@ const Hero = () => {
           alt="Hero Image"
           width={300}
           height={200}
+          priority
+          style={{ width: "auto", height: "auto" }}
           className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
         />
         <div className="hero-content opacity-0">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import MessageSection from "./components/MessageSection";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <MessageSection />
     </div>
   );
 }
